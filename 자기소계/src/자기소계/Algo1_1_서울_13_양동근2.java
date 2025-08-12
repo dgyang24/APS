bd78c8a2056d3ff924c335508c61b5e39556930c
dgyang24 <donggeuny24@gmail.com>
2025-08-12 16:09:44 +0900

    day07

---
+++ 자기소계/src/자기소계/Algo1_1_서울_13_양동근2.java
@@ -0,0 +1,34 @@
+package 자기소계;
+
+import java.util.Scanner;
+
+public class Algo1_1_서울_13_양동근2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+
+        for (int tc = 1; tc <= T; tc++) {
+            String input = sc.next().toUpperCase();
+            StringBuilder sb = new StringBuilder();
+            int count = 1;
+
+            for (int i = 0; i < input.length() - 1; i++) {
+                char tmp = input.charAt(i);
+                char tmp2 = input.charAt(i + 1);
+
+                if (tmp == tmp2) {
+                    count++;
+                } else {
+                    sb.append(tmp).append(count);
+                    count = 1;
+                }
+            }
+
+            // 마지막 문자 처리
+            sb.append(input.charAt(input.length() - 1)).append(count);
+
+            System.out.println("#" + tc + " " + sb);
+        }
+    }
+}
+
--- 자기소계/src/자기소계/Algo1_서울_13_양동근.java
+++ 자기소계/src/자기소계/Algo1_서울_13_양동근.java
@@ -1,34 +1,76 @@
 package 자기소계;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 
 public class Algo1_서울_13_양동근 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int T = sc.nextInt();
+	static StringBuilder ans;
+	
+	public static void main(String[] args) throws FileNotFoundException{
+//		File file = new File("./src/문제1번/input.txt");
+//		Scanner sc = new Scanner(file);
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		// 65 - A + 32
+		// 97 - a 
+		int A = 65;
+		int a = 97;
+		char myAnswer = (char) (A + 32); // a
+		
+		for (int tc=1; tc<=T; tc++) {
+			ans = new StringBuilder();
+			
+			// 문자 입력받기 (String) + 대문자화
+			String s = sc.next().toUpperCase();
+			
+			// charAt
+			// n <- .length() 길이를
+			int n = s.length();
+			
+			// 투포인터 사용
+			int i = 0;
+			
+			ans = new StringBuilder();
+			// 문자의 길이만큼 반복
+			// while ( i < n)
+			// AAAABBCC
+			
+			while(i<n) {
+				// 기준이될 알파벳
+				char c = s.charAt(i);
+				
+				int j = i;
+				// 일단 나의 기준은 i 그래서 j를 i부터 탐색 시작
+				// j = i 
+				// j ~ 다른 알파벳 나올때까지 반복
+				// j ~ n의 길이까지 charAt(j)== charAt(i)
+				// while(j<n && charAt(j)==charAt(i)) 
+				// j를 하나씩 올려주기
+				
+				while(j<n && s.charAt(j)==c) {
+					j++;
+				}
+				
+				// ans 한 알파벳을 몇번 카운팅 했는지 적립
+				// .append 활용해서 
+				// 알파벳 써주고
+				// 그 알파벳 몇번 나왔는지 적립 (j-i)
+				ans.append(c)
+					.append(j-i);
 
-        for (int tc = 1; tc <= T; tc++) {
-            String input = sc.next().toUpperCase();
-            StringBuilder sb = new StringBuilder();
-            int count = 1;
+				// 01234567    
+				// AAAAAAABCC
+			   // i =0 
+			   // i = j // 로 바꿔주면 됨
+				i = j;
+				
+			}
+			System.out.println("#" + tc + " " + ans);
+		}
+		sc.close();
+	}
+	
 
-            for (int i = 0; i < input.length() - 1; i++) {
-                char tmp = input.charAt(i);
-                char tmp2 = input.charAt(i + 1);
-
-                if (tmp == tmp2) {
-                    count++;
-                } else {
-                    sb.append(tmp).append(count);
-                    count = 1;
-                }
-            }
-
-            // 마지막 문자 처리
-            sb.append(input.charAt(input.length() - 1)).append(count);
-
-            System.out.println("#" + tc + " " + sb);
-        }
-    }
 }
-
